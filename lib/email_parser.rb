18d81cac23b31615809fc83992ac3be206df6c74
sparkbold-git <sparkboldstudio@gmail.com>
2018-09-03 22:22:01 -0500

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,14 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+class EmailParser
+  attr_accessor :csv_data
+
+  def initialize(csv_data)
+    @csv_data = csv_data
+  end
+
+  def parse
+    parse_data = @csv_data.split(/[,\s]+/).uniq
+  end
+end
